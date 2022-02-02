dd4850fa0accdf498c1f41e218249de65a0f4676
JALkfjklsajr3iowejkdsl <97200171+JALkfjklsajr3iowejkdsl@users.noreply.github.com>
2022-02-02 17:57:01 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+/*
+Pacman
+La idea principal
+ de nuestro personaje principal 
+ es recorreor un laberinto mostrando en pantalla
+  lo cual debemos consumir los puntitos que se encuentran alrededor del mapa,
+   he irnos cuidando de los enemigos que se encuentran dianbulando por todo el mapa,
+   y si tenemos la mala suerte de que nos agarren perdemos una vida(tienes 3 vidas)
+*/
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+    background("black");
+}
